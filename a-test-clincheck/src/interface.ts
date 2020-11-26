aaf6a168726acbdcee68b47ef8205fd231aca7bd
Alexey Shmelkov <vzhik3268@gmail.com>
2020-11-26 17:25:33 +0300

    module type

--- a-test-clincheck/src/interface.ts
+++
@@ -1,4 +0,0 @@
-export interface A {
-  A1: string;
-  A2: string;
-}
--- c-test-clincheck/src/index.ts
+++
@@ -1,7 +0,0 @@
-
-import { AB } from '@clincheck/b-test-clincheck';
-
-const message: AB = { fromA: { A1: '1', A2: 'sdf' } , fromB: 1 }; //fromB muts be string!
-
-console.log(message.fromB);
-
