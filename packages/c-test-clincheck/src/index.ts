a8ded5bd40ab7af8cf7cb1db04f6254e6fb27d34
Alexey Shmelkov <vzhik3268@gmail.com>
2020-11-27 12:44:17 +0300

    add: yarn 2; ts project references

--- packages/c-test-clincheck/src/index.ts
+++ packages/c-test-clincheck/src/index.ts
@@ -1,7 +1,7 @@
 
 import { AX } from 'b-test-clincheck';
 
-const message: AX = { A: { A1: '1', A2: 'sdf' } , X: 1 }; //X muts be string
+const message: AX = { A: { A1: '1', A2: 'sdf' } , X: '1' }; //X muts be string
 
 console.log(message.X);
 
